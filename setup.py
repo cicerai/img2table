d82a33d4ca8a7995985c7bf46e3b1494b6f1cab4
pablorilo <prilom1984@gmail.com>
2023-11-30 14:08:51 +0100

    build(setups): modify setups so that it performs the release correctly

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-
 requirements = []
-
 setup(
     package_dir={'': 'src'},
     install_requires=requirements,
     include_package_data=True,
     keywords='img2table',
     name='img2table',
-    packages=find_packages(include=['img2table', 'img2table.*']),
+    packages=find_packages(where='src'),
     test_suite='tests',
-    version='0.1.2',
+    version='0.0.2',
     zip_safe=False,
 )
